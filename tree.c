52e5198873999c9c1f3346df07cad8ab35dd3b0e
Gilles Leblanc <gilles.leblanc@gmail.com>
2015-06-11 21:22:14 -0400

    Adds depth_first_pre and _post

--- tree.c
+++ tree.c
@@ -8,24 +8,26 @@ struct tree_node {
 };
 
 typedef struct tree_node node;
+typedef void (*node_func)(node *);
 
 node * create_node(int, node *);
-void build_tree(node *);
-void free_tree(node *);
+node * build_tree();
+void depth_first_pre(node *, node_func);
+void depth_first_post(node *, node_func);
+void print_node(node *);
+void free_node(node *);
 
 int main() {
-  node *root = malloc(sizeof(*root));
+  node *root = build_tree();
 
-  build_tree(root);
+  depth_first_pre(root, print_node);
 
-  /* TODO: function that frees the tree */
-  free_tree(root);
-  /* free(root); */
+  depth_first_post(root, free_node);
   return 0;
 }
 
 node * create_node(int value, node *parent) {
-  node *new = malloc(sizeof(*new));
+  node *new = malloc(sizeof(node));
   new->value = value;
   new->parent = parent;
 
@@ -33,8 +35,8 @@ node * create_node(int value, node *parent) {
 }
 
 /* builds a tree with a preset of data */
-void build_tree(node *root) {
-  root = create_node(2, NULL);
+node * build_tree() {
+  node *root = create_node(2, NULL);
     root->left = create_node(7, root);
       root->left->left = create_node(2, root->left);
       root->left->right = create_node(6, root->left);
@@ -43,8 +45,40 @@ void build_tree(node *root) {
     root->right = create_node(5, root);
       root->right->right = create_node(9, root->right);
         root->right->right->left = create_node(4, root->right->right);
+
+  return root;
 }
 
-/* recursively frees the tree */
-void free_tree(node *root) {
+/* function that walks tree and takes function pointer */
+/* Pre-order Depth-first traversal */
+void depth_first_pre(node *current, node_func func) {
+  if (current == NULL) {
+    return;
+  }
+
+  (func(current));
+
+  depth_first_pre(current->left, func);
+  depth_first_pre(current->right, func);
+}
+
+/* function that walks tree and takes function pointer */
+/* Post-order Depth-first traversal */
+void depth_first_post(node *current, node_func func) {
+  if (current == NULL) {
+    return;
+  }
+
+  depth_first_post(current->left, func);
+  depth_first_post(current->right, func);
+
+  (func(current));
+}
+
+void print_node(node *current) {
+  printf("%d, ", current->value);
+}
+
+void free_node(node *current) {
+  free(current);
 }
